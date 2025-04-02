8226dad3076b1f8a5d6c9069cc5bbff8e4365329
Zhalslar <zsl2936169201@gmail.com>
2025-04-03 02:29:05 +0800

    支持自定义音色和情绪

--- README.md
+++ README.md
@@ -7,11 +7,12 @@
 
 ## 📦 安装
 
-- 第一步，本地部署 GPT_SoVITS（安装包大约5G，装完后10G以上）、
-- 本人用的是B站up主做的整合包: [2小时轻松入门GPT-SoVITS，包含整合包，autodl，colab教程，搭配文档观看](https://www.bilibili.com/video/BV1GJ4m1e7x2/?share_source=copy_web&vd_source=b3e26d110d9269b5607f8a2e9ffb7345)
-- 安装过程遇到的各种问题，如非本插件范畴内，请自行解决
+### 第一步，本地部署 GPT_SoVITS、
+- 安装步骤请看[GPT_SoVITS仓库](https://github.com/RVC-Boss/GPT-SoVITS)（安装包6G+，装完后10G+）
+- 配合[GPT_SoVITS指南](https://www.yuque.com/baicaigongchang1145haoyuangong/ib3g1e)来看
 
-- 第二步，安装本插件
+
+### 第二步，安装本插件
 - 可以直接在astrbot的插件市场搜索astrbot_plugin_GPT_SoVITS，点击安装，耐心等待安装完成即可  
 
 - 或者可以直接克隆源码到插件文件夹：
@@ -29,25 +30,23 @@ git clone https://github.com/Zhalslar/astrbot_plugin_GPT_SoVITS
 ## ⚙️ 配置
  
 请在astrbot面板配置，插件管理 -> astrbot_plugin_memelite -> 操作 -> 插件配置
-![tmpE999](https://github.com/user-attachments/assets/2f101895-a28f-45d7-8707-84f46ea3d990)
 
 
 ## 🐔 使用说明
-- 正常聊天，有概率自动触发LLM文本转语音，概率可在配置里更改
-
 - `{emotion} {text}` - 生成语音，emotion为情绪，text为文本
 - `惊讶地说 怎么啦？` - 示例1，注意用空格隔空参数
 - `生气地说 我再也不理你了` - 示例2，注意用空格隔空参数
 
 ## 📌 注意事项
-GPT_SoVITS的部署目前我仅在Windows11下测试过，并成功部署，更多环境下的部署请看[GPT_SoVITS官方文档](https://github.com/RVC-Boss/GPT-SoVITS/blob/main/docs/cn/README.md)
-或者本人用的[整合包文档](https://www.yuque.com/xter/zibxlp/nqi871glgxfy717e#)
+- 本项目优先兼容官方整合包，第三方整合包只要不是大改的基本也能对接
+- GPT_SoVITS的部署目前我仅测试了Windows环境，更多环境下的部署请自行查阅[GPT_SoVITS官方文档](https://github.com/RVC-Boss/GPT-SoVITS/blob/main/docs/cn/README.md)
+- 想第一时间得到反馈的可以来作者的插件反馈群（QQ群）：460973561
 
-
-## 📌 TODO
-- 适配GPT_SoVITS_v3
+## TODO
 - 测试更多环境下的部署
 
 ## 📜 开源协议
 本项目采用 [MIT License](LICENSE)
+
+
 
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 import os
 import re
 from datetime import datetime
@@ -20,84 +22,122 @@ REFERENCE_AUDIO_DIR: Path =  Path(__file__).resolve().parent / "reference_audio"
 SAVED_AUDIO_DIR.mkdir(parents=True, exist_ok=True)
 REFERENCE_AUDIO_DIR.mkdir(parents=True, exist_ok=True)
 
-# 预设情绪
-preset_emotion: Dict = {
-    "温柔地说": {
-        "ref_audio_path": REFERENCE_AUDIO_DIR / "不要害怕，也不要哭了.wav",
-        "prompt_text": "不要害怕，也不要哭了",
-        "prompt_lang": "zh",
-        "speed_factor": 1,
-        "fragment_interval": 0.7,
-    },
-    "开心地说": {
-        "ref_audio_path": REFERENCE_AUDIO_DIR / "它好像在等另一只蕈兽_心情很好的样子.wav",
-        "prompt_text": "它好像在等另一只蕈兽，心情很好的样子",
-        "prompt_lang": "zh",
-        "speed_factor": 1.1,
-        "fragment_interval": 0.3,
-    },
-    "生气地说": {
-        "ref_audio_path": REFERENCE_AUDIO_DIR / "你还会选择现在的位置吗？到那时，你觉得自己又会是什么呢.wav",
-        "prompt_text": "你还会选择现在的位置吗？到那时，你觉得自己又会是什么呢",
-        "prompt_lang": "zh",
-        "speed_factor": 1.2,
-        "fragment_interval": 0.5,
-    },
-    "惊讶地说": {
-        "ref_audio_path": REFERENCE_AUDIO_DIR / "就算是这样，也不至于直接碎掉啊，除非.wav",
-        "prompt_text": "就算是这样，也不至于直接碎掉啊，除非",
-        "prompt_lang": "zh",
-        "speed_factor": 1,
-        "fragment_interval": 0.6,
-    }
-}
 
-preset_emotion_set = set(preset_emotion.keys())
 
 
 @register("astrbot_plugin_GPT_SoVITS", "Zhalslar", "GPT_SoVITS对接插件", "1.0.0", "https://github.com/Zhalslar/astrbot_plugin_GPT_SoVITS")
 class GPTSoVITSPlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
-        self.base_url: str = config.get('base_url')
-        self.default_emotion: str =  config.get("default_emotion","惊讶地")  # 默认情绪预设
-        self.save_audio: bool = config.get("save_audio", False) # 是否保存合成的音频
+        self.base_url: str = config.get('base_setting').get('base_url')
+        self.save_audio: bool = config.get('base_setting').get("save_audio") # 是否保存合成的音频
         self.save_path = None
-        self.send_record_probability: float = config.get("send_record_probability", 0.15)  # 发语音的概率
-        self.default_tts_params: Dict = {
-            "ref_audio_path": config.get('ref_audio_path',""),  # 参考音频文件路径
-            "text": config.get('text', ""),  # 要转换的文本
-            "text_lang": config.get('text_lang', "zh"),  # 文本语言，默认为中文
-            "aux_ref_audio_paths": config.get('prompt_text', None),   # 辅助参考音频文件路径列表
-            "prompt_text": config.get('prompt_text', ""),  # 提示文本，用于影响语音合成
-            "prompt_lang": config.get('prompt_lang', "zh"),  # 提示文本的语言，默认为中文
-            "top_k": config.get('top_k', 5),  # 控制生成语音的多样性
-            "top_p": config.get('top_p', 1.0),  # 核采样的阈值
-            "temperature": config.get('temperature', 1.0),  # 控制生成语音的随机性
-            "text_split_method": config.get('text_split_method', "cut3"),  # 文本分割的方法
-            "batch_size": config.get('batch_size', 1),  # 批处理大小
-            "batch_threshold": config.get('batch_threshold', 0.75),  # 批处理阈值
-            "split_bucket": config.get('split_bucket', True),  # 是否将文本分割成桶以便并行处理
-            "speed_factor": config.get('speed_factor', 1),  # 语音播放速度的倍数
-            "fragment_interval": config.get('fragment_interval', 0.3),  # 语音片段之间的间隔时间
-            "streaming_mode": config.get('streaming_mode', False),  # 是否启用流模式
-            "seed": config.get('seed', -1),  # 随机种子，用于结果的可重复性
-            "parallel_infer": config.get('parallel_infer', True),  # 是否并行执行推理
-            "repetition_penalty": config.get('repetition_penalty', 1.35),  # 重复惩罚因子
-            "media_type": config.get('media_type', 'wav'),  # 输出媒体的类型
+        self.send_record_probability: float = config.get('base_setting').get("send_record_probability")  # 发语音的概率
+
+        self.default_emotion: str = config.get('role').get("default_emotion")  # 默认情绪预设
+        self.gpt_weights_path: str = config.get('role', {}).get('gpt_weights_path')  # GPT模型文件路径
+        self.sovits_weights_path: str = config.get('role', {}).get('sovits_weights_path')  # SoVITS模型文件路径
+        self._set_model_weights()
+
+        self.gently_config = config.get('emotions', {}).get('gently', {})
+        self.happily_config = config.get('emotions', {}).get('happily', {})
+        self.angrily_config = config.get('emotions', {}).get('angrily', {})
+        self.surprise_config = config.get('emotions', {}).get('surprise', {})
+        self.preset_emotions = {
+            "温柔地说": {
+                "ref_audio_path": self.gently_config.get('ref_audio_path') or str(REFERENCE_AUDIO_DIR / "不要害怕，也不要哭了.wav"),
+                "prompt_text": self.gently_config.get('prompt_text') or "不要害怕，也不要哭了",
+                "prompt_lang": self.gently_config.get('prompt_lang'),
+                "speed_factor": self.gently_config.get('speed_factor'),
+                "fragment_interval": self.gently_config.get('fragment_interval'),
+            },
+            "开心地说": {
+                "ref_audio_path": self.happily_config.get('ref_audio_path') or str(REFERENCE_AUDIO_DIR / "它好像在等另一只蕈兽_心情很好的样子.wav"),
+                "prompt_text":  self.gently_config.get('prompt_text') or "它好像在等另一只蕈兽_心情很好的样子",
+                "prompt_lang": self.happily_config.get('prompt_lang'),
+                "speed_factor": self.happily_config.get('speed_factor'),
+                "fragment_interval": self.happily_config.get('fragment_interval'),
+            },
+            "生气地说": {
+                "ref_audio_path": self.angrily_config.get('ref_audio_path') or str(REFERENCE_AUDIO_DIR / "你还会选择现在的位置吗？到那时，你觉得自己又会是什么呢.wav"),
+                "prompt_text":  self.gently_config.get('prompt_text') or "你还会选择现在的位置吗？到那时，你觉得自己又会是什么呢",
+                "prompt_lang": self.angrily_config.get('prompt_lang'),
+                "speed_factor": self.angrily_config.get('speed_factor'),
+                "fragment_interval": self.angrily_config.get('fragment_interval'),
+            },
+            "惊讶地说": {
+                "ref_audio_path": self.surprise_config.get('ref_audio_path') or str(REFERENCE_AUDIO_DIR / "就算是这样，也不至于直接碎掉啊，除非.wav"),
+                "prompt_text": self.gently_config.get('prompt_text') or "就算是这样，也不至于直接碎掉啊，除非",
+                "prompt_lang": self.surprise_config.get('prompt_lang'),
+                "speed_factor": self.surprise_config.get('speed_factor'),
+                "fragment_interval": self.surprise_config.get('fragment_interval'),
+            }
         }
+        self.preset_emotions_set = set(self.preset_emotions.keys())
+
+
+        self.params = config.get('default_params')
+        self.default_params: Dict = {
+            "ref_audio_path": self.params.get('ref_audio_path',""),  # 参考音频文件路径
+            "text": self.params.get('text', ""),  # 要转换的文本
+            "text_lang": self.params.get('text_lang', "zh"),  # 文本语言，默认为中文
+            "aux_ref_audio_paths": self.params.get('prompt_text', None),   # 辅助参考音频文件路径列表
+            "prompt_text": self.params.get('prompt_text', ""),  # 提示文本，用于影响语音合成
+            "prompt_lang": self.params.get('prompt_lang', "zh"),  # 提示文本的语言，默认为中文
+            "top_k": self.params.get('top_k', 5),  # 控制生成语音的多样性
+            "top_p": self.params.get('top_p', 1.0),  # 核采样的阈值
+            "temperature": self.params.get('temperature', 1.0),  # 控制生成语音的随机性
+            "text_split_method": self.params.get('text_split_method', "cut3"),  # 文本分割的方法
+            "batch_size": self.params.get('batch_size', 1),  # 批处理大小
+            "batch_threshold": self.params.get('batch_threshold', 0.75),  # 批处理阈值
+            "split_bucket": self.params.get('split_bucket', True),  # 是否将文本分割成桶以便并行处理
+            "speed_factor": self.params.get('speed_factor', 1),  # 语音播放速度的倍数
+            "fragment_interval": self.params.get('fragment_interval', 0.3),  # 语音片段之间的间隔时间
+            "streaming_mode": config.get('streaming_mode', False),  # 是否启用流模式
+            "seed": self.params.get('seed', -1),  # 随机种子，用于结果的可重复性
+            "parallel_infer": self.params.get('parallel_infer', True),  # 是否并行执行推理
+            "repetition_penalty": config.get('repetition_penalty', 1.35),  # 重复惩罚因子
+            "media_type": self.params.get('media_type', 'wav'),  # 输出媒体的类型
+        }
+
+    def _set_model_weights(self):
+        """设置模型权重路径"""
+        try:
+            # 设置 GPT 模型权重
+            if self.gpt_weights_path:
+                gpt_endpoint = f"{self.base_url}/set_gpt_weights"
+                gpt_params = {"weights_path": self.gpt_weights_path}
+                response = requests.get(gpt_endpoint, params=gpt_params)
+                response.raise_for_status()
+                logger.info(f"成功设置 GPT 模型权重路径：{self.gpt_weights_path}")
+            else:
+                logger.info("GPT 模型权重路径未配置，将使用GPT_SoVITS内置的GPT模型")
+
+            # 设置 SoVITS 模型权重
+            if self.sovits_weights_path:
+                sovits_endpoint = f"{self.base_url}/set_sovits_weights"
+                sovits_params = {"weights_path": self.sovits_weights_path}
+                response = requests.get(sovits_endpoint, params=sovits_params)
+                response.raise_for_status()
+                logger.info(f"成功设置 SoVITS 模型权重路径：{self.sovits_weights_path}")
+            else:
+                logger.info("SoVITS 模型权重路径未配置，将使用GPT_SoVITS内置的SoVITS模型")
+        except requests.RequestException as e:
+            logger.error(f"设置模型权重路径时发生错误：{e}")
+
+
 
     async def generate_and_send_tts(self, event: AstrMessageEvent, text: str, emotion: str):
         """生成TTS语音并发送"""
         group_id = event.get_group_id()
         sender_id = event.get_sender_id()
 
-        params = self.default_tts_params.copy()
-        params.update(preset_emotion[emotion])
+        params = self.default_params.copy()
+        params.update(self.preset_emotions[emotion])
         params["text"] = text
 
         file_name = self.generate_file_name(params=params, group_id=group_id, user_id=sender_id)
-        await self.tts_run(params=params, file_name=file_name)
+        await self.tts_inference(params=params, file_name=file_name)
 
         if self.save_path is None:
             logger.error("TTS任务执行失败！")
@@ -123,12 +163,18 @@ class GPTSoVITSPlugin(Star):
         await self.generate_and_send_tts(event, text, emotion)
 
 
-    @filter.command("说", alias=preset_emotion_set)
+    @filter.command("说", alias={"温柔地说", "开心地说", "生气地说", "惊讶地说", })
     async def on_regex(self, event: AstrMessageEvent, text: str = None):
         """xxx说xxx，直接调用TTS，发送合成后的语音"""
         message_str = event.get_message_str()
 
-        emotion = next((emo for emo in preset_emotion_set if emo in message_str), self.default_emotion)
+        emotion = next((emo for emo in self.preset_emotions_set if emo in message_str), self.default_emotion)
+
+        params = self.default_params.copy()
+        params.update(self.preset_emotions[emotion])
+        params["text"] = text
+
+        yield event.plain_result(f"{params}")
 
         if not emotion or not text:
             return
@@ -140,16 +186,16 @@ class GPTSoVITSPlugin(Star):
         """生成文件名"""
         sanitized_text = re.sub(r'[^a-zA-Z0-9\u4e00-\u9fff\s]', '', params["text"])
         limit_text = sanitized_text[:30]  # 限制长度
-        media_type = self.default_tts_params["media_type"]
+        media_type = self.default_params["media_type"]
         current_time = datetime.now().strftime("%Y%m%d_%H%M%S")  # 格式化为年月日_时分秒
         file_name = f"{group_id}_{user_id}_{limit_text}_{current_time}.{media_type}"
         return file_name
 
 
-    async def tts_run(self, params, file_name: str = None) -> str | None:
+    async def tts_inference(self, params, file_name: str = None) -> str | None:
         """发送TTS请求，获取音频内容"""
-        print(params)
-        response = requests.get(self.base_url, params=params)
+        endpoint = f"{self.base_url}/tts"
+        response = requests.get(endpoint, params=params)
         if response.status_code != 200:
             return None
         audio_bytes: bytes = response.content
@@ -159,11 +205,12 @@ class GPTSoVITSPlugin(Star):
         return self.save_path
 
 
-
-
-
-
-
+    @filter.command("重启TTS", alias={"重启tts"})
+    async def tts_control(self,event: AstrMessageEvent):
+        yield event.plain_result(f"重启TTS中...(报错信息请忽略，等待一会即可完成重启)")
+        endpoint = f"{self.base_url}/control"
+        params = {"command": "restart"}
+        requests.get(endpoint, params=params)
 
 
 
